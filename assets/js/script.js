7660aac3ff66909d82f3024e14bc6c903ef0d459
Turaldi <91114292+Turaldi@users.noreply.github.com>
2022-12-07 00:36:43 +0400

    added slider container

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// SLIDER CONTAINER START
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        // autoplay:true,
+        // autoplaySpeed:1000,
+        smartSpeed:1500,
+        autoplayHoverPause:true 
+
+
+    });
+})
+// SLIDER CONTAINER END
