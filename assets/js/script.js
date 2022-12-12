bf7ab475a078211f16224c1dc8acb136c3f01224
Turaldi <91114292+Turaldi@users.noreply.github.com>
2022-12-12 20:45:42 +0400

    register and login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // SLIDER CONTAINER START
-$(document).ready(function(){
+$(document).ready(function () {
     $(".first-slider").owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
         // autoplay:true,
         // autoplaySpeed:1000,
-        smartSpeed:1500,
-        autoplayHoverPause:true 
+        smartSpeed: 1500,
+        autoplayHoverPause: true
 
 
     });
@@ -18,41 +18,34 @@ $(document).ready(function(){
 
 function updateTimer() {
     future = Date.parse("jun 12, 2022 01:30:00");
- now = new Date();
- diff = future - now;
+    now = new Date();
+    diff = future - now;
 
- days = Math.floor(diff / (1000 * 60 * 60 * 24));
- hours = Math.floor(diff / (1000 * 60 * 60));
- mins = Math.floor(diff / (1000 * 60));
- secs = Math.floor(diff / 1000);
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
 
- d = days;
- h = hours - days * 24;
- m = mins - hours * 60;
- s = secs - mins * 60;
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
 
- document.getElementById("timer")
-  .innerHTML =
-  '<div>' + d + '</div>' +
-  '<div>' + h + '</div>' +
-  '<div>' + m + '</div>' +
-  '<div>' + s + '</div>';
+    document.getElementById("timer")
+        .innerHTML =
+        '<div>' + d + '</div>' +
+        '<div>' + h + '</div>' +
+        '<div>' + m + '</div>' +
+        '<div>' + s + '</div>';
 }
 setInterval('updateTimer()', 1000);
-
-
-
-// $('.homeSlider').slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     arrows:true,
-//     dots:true
-//   });
-
 
 $('.homeSlider-item').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 3
-  });
+});
+
+
+
+
